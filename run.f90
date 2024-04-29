3df930c7c21aeb5591b698772568a530b4264b1e
Alex Buccheri <alexanderbuccheri@googlemail.com>
2024-04-29 15:28:23 +0100

    Modify to run with and without MPI. - Serial and single MPI process give consistent results.

--- run.f90
+++ run.f90
@@ -6,9 +6,14 @@ program run
     use, intrinsic :: iso_fortran_env, only: dp => real64
     use omp_lib
 
+    use mpi_m, only: mpi_t, mpi_init, MPI_Finalize, distribute_elements_start_stop
     use grids_m,  only: linspace, linspace_to_grid
     use kmeans_m, only: weighted_kmeans
     implicit none
+
+    ! MPI
+    type(mpi_t), allocatable :: comm
+    integer, allocatable :: start_end_indices(:, :), ir_indices(:) !< Indices for data distributed on the grid
 
     ! Files
     ! Root
@@ -27,11 +32,18 @@ program run
     real(dp), allocatable :: weight(:)
     real(dp), allocatable :: centroids(:, :)
 
-    integer :: ir, ic, niter
+    integer :: ir, ic, niter, ierr, i
     logical :: verbose
 
+    ! Initialise MPI env
+    call mpi_init(ierr)
+    comm = mpi_t() 
 
-   ! Check if a command-line argument is provided
+    write(*, *) 'comm got initialised'
+
+    ! For convenience, have each process do its own IO
+
+    ! Check if a command-line argument is provided
     if (command_argument_count() > 0) then
         ! Retrieve the first command-line argument
         call get_command_argument(1, root)
@@ -47,6 +59,8 @@ program run
         root = trim(default_root)
     end if
     
+    write(*, *) 'Parse command line'
+
     ! Parse grid settings
     file = trim(adjustl(root)) // "jupyter/grid_settings.dat"
     open(newunit=fid, file=trim(adjustl(file)))
@@ -56,15 +70,20 @@ program run
     read(fid, *) ranges(:, 2), sampling(2)
     close(fid)
 
+    write(*, *) 'Parse grid settings'
+
     ! Generate grid from this
     nr = product(sampling)
     allocate(grid(n_dim, nr))
     call construct_2d_grid(sampling, ranges, grid)
 
     ! Print grid. Looks consistent with python setup
-    ! do ir = 1, nr
-    !     write(*, *) grid(:, ir)
-    ! enddo
+    if (comm%is_root()) then
+        write(*, *) 'Outputting grid'
+        do ir = 1, nr
+            write(201, *) grid(:, ir)
+        enddo
+    endif
 
     ! Parse weights
     allocate(weight(nr))
@@ -74,6 +93,11 @@ program run
         read(fid, *) weight(ir)
     enddo
     close(fid)
+
+    ! Distribute grid and weight - quantities on the grid
+    allocate(start_end_indices(2, comm%np))
+    start_end_indices = distribute_elements_start_stop(comm%np, nr)
+    ir_indices = [(i, i= start_end_indices(1, comm%rank+1), start_end_indices(2, comm%rank+1))]
 
     ! Parse initial centroids
     file = trim(adjustl(root)) // "jupyter/init_centroids.dat"
@@ -87,16 +111,21 @@ program run
 
     niter = 50
     verbose = .true.
-    call weighted_kmeans(grid, weight, centroids, n_iter=niter, verbose=verbose)
 
-    ! Output final centroids
-    file = trim(adjustl(root)) // "jupyter/final_centroids.dat"
-    open(newunit=fid, file=trim(adjustl(file)), action='write')
-    write(fid, *) n_centroids
-    do ic = 1, n_centroids
-        write(fid, *) centroids(:, ic)
-    enddo
-    close(fid)
+    call weighted_kmeans(comm, grid(:, ir_indices), weight(ir_indices), centroids, n_iter=niter, verbose=verbose)
+
+    ! Output final centroids from root
+    if (comm%is_root()) then
+        file = trim(adjustl(root)) // "jupyter/final_centroids.dat"
+        open(newunit=fid, file=trim(adjustl(file)), action='write')
+        write(fid, *) n_centroids
+        do ic = 1, n_centroids
+            write(fid, *) centroids(:, ic)
+        enddo
+        close(fid)
+    endif
+
+    call mpi_finalize(ierr)
 
 contains
     subroutine construct_2d_grid(sampling, ranges, grid)
